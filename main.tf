c5a53a11351796224e03f056c7a2a18d077fd605
likhitha-10 <gangenanilikhitha@gmail.com>
2024-10-19 10:26:16 +1100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           = "ami-0f71013b2c8bd2c29"
   instance_type = "t2.micro"
-  key_name="demokey"
   vpc_security_group_ids=["sg-0d2c5800c5a037018"]
   tags = {
     Name = "jenkins-pipeline"
