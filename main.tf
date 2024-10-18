cceaefc7c06c502c910f3308e911b89c172ed03e
likhitha-10 <gangenanilikhitha@gmail.com>
2024-10-19 00:04:24 +1100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "ap-southeast-2" 
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0f71013b2c8bd2c29"
+  instance_type = "t2.micro"
+  key_name="demokey"
+  vpc_security_group_ids=["sg-0d2c5800c5a037018"]
+  tags = {
+    Name = "jenkins-pipeline"
+  }
+}
+
+
